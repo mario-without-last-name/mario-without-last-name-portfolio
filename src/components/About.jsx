2f2dbc75e01c0826b3a4fdc1ec00a07ba0ba02c9
Mario <mario007@binus.ac.id>
2024-11-26 11:56:44 +0700

    fixing mariodiamond image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc'
-import mario from '../assets/mariodiamond.png';
+import mario from '../assets/mario.png';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
