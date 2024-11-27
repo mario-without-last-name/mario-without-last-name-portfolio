da64cce5d653752672f38cb332fe46812acb0305
Mario <mario007@binus.ac.id>
2024-11-27 18:38:08 +0700

    tiny 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { ComputersCanvas } from "./canvas";
 
+import downloadIcon from "../assets/download.png"
+
 const Hero = () => {
   return (
     <section className="relative w-full h-[375px] sm:h-screen mx-auto">
@@ -24,8 +26,8 @@ const Hero = () => {
           {/* Button for downloading the resume */}
           <div className="mt-4 z-10 relative">
             <a href="https://binusianorg-my.sharepoint.com/personal/mario007_binus_ac_id/_layouts/15/guestaccess.aspx?share=EZPdrFSdzlFAvLB9-IOw6nwBmDR5iarSJuadKX7HDboIYw&e=uMZLw0" target="_blank" rel="noopener noreferrer">
-              <button className="px-6 py-3 bg-[#915eff] text-white rounded-md hover:bg-[#7a4ed6] transition duration-300">
-                Download my resume
+            <button className="px-6 py-3 bg-[#915eff] text-white rounded-md hover:bg-[#7a4ed6] transition duration-300 flex items-center">
+                <img src={downloadIcon} alt="" className="w-5 h-5 mr-2"/> Download my resume
               </button>
             </a>
           </div>
