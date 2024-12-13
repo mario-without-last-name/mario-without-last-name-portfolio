8cd242b069f45aee723416d52e21ea0837fce795
Mario <mario007@binus.ac.id>
2024-12-13 15:25:14 +0700

    attempt to revert github pages fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mario-without-last-name-portfolio/"
+  // base: "/mario-without-last-name-portfolio/"
 })
