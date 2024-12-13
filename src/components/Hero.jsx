f361b8b2efc78804644ce83d754776744219ac7e
Mario <mario007@binus.ac.id>
2024-12-13 17:08:10 +0700

    update resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
 
           {/* Button for downloading the resume */}
           <div className="mt-4 z-10 relative">
-            <a href="https://binusianorg-my.sharepoint.com/personal/mario007_binus_ac_id/_layouts/15/guestaccess.aspx?share=EY7tlNJO8mNFpQbE5PkRSpQBt3jDsobFV2zRdM6IjH-Wag&e=7Gicw8" target="_blank" rel="noopener noreferrer" className="inline-block">
+            <a href="https://binusianorg-my.sharepoint.com/personal/mario007_binus_ac_id/_layouts/15/guestaccess.aspx?share=EWwevwF7ms5AjYwdjSYvVr4BFm4GWneBjGLwWPRH8iEFdA&e=R9ijCH" target="_blank" rel="noopener noreferrer" className="inline-block">
               <button className="px-6 py-3 bg-[#915eff] text-white rounded-md hover:bg-[#7a4ed6] transition duration-300 flex items-center">
                 <img src={downloadIcon} alt="" className="w-5 h-5 mr-2"/> 
                 <span className="hidden sm:inline">Download my resume</span>
